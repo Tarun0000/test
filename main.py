c3ad9d9aad66fc2d484cb44bdad741f35ba40686
Mr Tarun Sonwane <tarunsonwane140@gmail.com>
2022-08-10 11:11:17 +0530

    new branch

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("this is add line")
     print("thsi is another linke  after cpmmit")
+    print("ythis add in new branch")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
